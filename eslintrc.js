81776644372e209c7c3f5bb1a2731d23235fec41
Paul Varache <paul@kano.me>
2018-09-26 14:15:15 +0100

    Extend @kano/eslint-config

--- eslintrc.js
+++ eslintrc.js
@@ -1,20 +1,5 @@
 /* Kano Computing Web Team eslint base configuration */
 
 module.exports = {
-    env: {
-        browser: true,
-        node: true
-    },
-    extends: require.resolve('eslint-config-airbnb-base'),
-    rules: {
-        indent: ['error', 4],
-        'no-underscore-dangle': ['error', { allowAfterSuper: true, allowAfterThis: true }],
-        'import/extensions': [2, 'always', { ignorePackages: true }],
-        "class-methods-use-this": ["off"],
-        "no-bitwise": ["off"],
-    },
-    plugins: [
-        'eslint-plugin-html',
-        'eslint-plugin-import',
-    ],
+    extends: require.resolve('@kano/eslint-config'),
 };
